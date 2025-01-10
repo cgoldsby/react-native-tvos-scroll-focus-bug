8abe38b1a46518386a8d6f632c4fe9709fbb6c1f
cgoldsby <cgoldsby@gmail.com>
2025-01-10 17:55:16 -0500

    Update App.tsx
    Update README.md

--- App.tsx
+++ App.tsx
@@ -4,115 +4,171 @@
  *
  * @format
  */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+import {useState} from 'react';
+import{
   StyleSheet,
+  SafeAreaView,
   Text,
-  useColorScheme,
+  LogBox,
+  TVTextScrollView,
   View,
+  Pressable,
+  PressableProps,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+LogBox.ignoreLogs([
+  'Sending `appearanceChanged` with no listeners registered.',
+]);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import {FunctionComponent} from 'react';
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Button: FunctionComponent<PressableProps & {text: string}> = props => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <Pressable
+      tvParallaxProperties={{enabled: false}}
+      onPress={props.onPress}
+      isTVSelectable={props.isTVSelectable}>
+      {({pressed, focused}) => (
+        <View style={[styles.button, focused && {backgroundColor: '#EF233C'}, !(props.isTVSelectable ?? true) && {backgroundColor: 'transparent'}, props.style]}>
+          <Text style={[styles.buttonText, focused && {color: '#EDF2F4'}, !(props.isTVSelectable ?? true) && {color: '#EDF2F4'}]}>
+            {props.text}
+          </Text>
+        </View>
+      )}
+    </Pressable>
   );
-}
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [scrollEnabled, setScrollEnabled] = useState(false);
+  const [longTextEnabled, setLongTextEnabled] = useState(true);
+  const [horizontalContent, setHorizontalContent] = useState(false);
+  const [headerFocusable, setHeaderFocusable] = useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const onEnableScrolling = () => {
+    setScrollEnabled(!scrollEnabled);
+  };
+
+  const onEnableLongText = () => {
+    setLongTextEnabled(!longTextEnabled);
+  };
+
+  const onEnableHorizontalContent = () => {
+    setHorizontalContent(!horizontalContent);
+  };
+
+  const onHeaderFocusable = () => {
+    setHeaderFocusable(!headerFocusable);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.listContainer}>
+        <Button
+          text={(scrollEnabled ? '' : 'Not ') + 'Scrollable'}
+          onPress={onEnableScrolling}
+        />
+        <Button
+          text={longTextEnabled ? 'Long' : 'Short'}
+          onPress={onEnableLongText}
+        />
+        <Button
+          text={horizontalContent ? 'Horizontal' : 'Vertical'}
+          onPress={onEnableHorizontalContent}
+        />
+        <Button
+          text={(headerFocusable ? ' ' : 'No ') + 'Focusable Boundaries'}
+          onPress={onHeaderFocusable}
+        />
+        <Button
+          text={'Reset'}
+          onPress={() => {
+            setScrollEnabled(false);
+            setLongTextEnabled(true);
+            setHorizontalContent(false);
+            setHeaderFocusable(false);
+          }}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Button text={'Header'} isTVSelectable={headerFocusable} />
+        <Text style={[styles.text, styles.textHeader]}>
+          {'Scrolling ' +
+            (scrollEnabled ? 'enabled' : 'disabled') +
+            ' / ' +
+            (longTextEnabled ? 'Long' : 'Short') +
+            ' text / ' +
+            (horizontalContent ? 'Horizontal' : 'Vertical') +
+            ' content'}
+        </Text>
+        <View style={{flex: 1, width:'100%'}}>
+          <TVTextScrollView
+            scrollEnabled={scrollEnabled}
+            horizontal={horizontalContent}>
+            <Text style={styles.text}>
+              {(!longTextEnabled && horizontalContent) ? 'Hello, world!' : longText.slice(0, longTextEnabled ? longText.length : 400) +
+                '\n\nThe end.'}
+            </Text>
+          </TVTextScrollView>
         </View>
-      </ScrollView>
+        <Button text={'Footer'} isTVSelectable={headerFocusable} />
+      </View>
+      <Button style={{height: '100%', width: 180}}text={'Right'} isTVSelectable={headerFocusable} />
+
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#2B2D42',
+    flexDirection: 'row',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  text: {
+    color: '#EDF2F4',
+    fontSize: 28,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  listContainer: {
+    width: '25%',
+    margin: 10,
+    gap: 50,
   },
-  highlight: {
-    fontWeight: '700',
+  textContainer: {
+    flex: 1,
+    alignItems: 'center',
+    gap: 20,
+  },
+  textHeader: {
+    fontWeight: 'bold',
+    fontSize: 32,
+  },
+  button: {
+    width: '80%',
+    borderRadius: 10,
+    backgroundColor: '#8D99AE',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#2B2D42',
+    fontSize: 38,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    padding: 38,
+  },
+  buttonPressed: {
+    backgroundColor: '#8D99AE',
   },
 });
 
 export default App;
+
+const longText = `
+"You see, Watson," he explained in the early hours of the morning as we sat over a glass of whisky and soda in Baker Street, "it was perfectly obvious from the first that the only possible object of this rather fantastic business of the advertisement of the League, and the copying of the 'Encyclopaedia,' must be to get this not over-bright pawnbroker out of the way for a number of hours every day. It was a curious way of managing it, but, really, it would be difficult to suggest a better. The method was no doubt suggested to Clay's ingenious mind by the colour of his accomplice's hair. The 4 pounds a week was a lure which must draw him, and what was it to them, who were playing for thousands? They put in the advertisement, one rogue has the temporary office, the other rogue incites the man to apply for it, and together they manage to secure his absence every morning in the week. From the time that I heard of the assistant having come for half wages, it was obvious to me that he had some strong motive for securing the situation."
+
+"But how could you guess what the motive was?"
+
+"Had there been women in the house, I should have suspected a mere vulgar intrigue. That, however, was out of the question. The man's business was a small one, and there was nothing in his house which could account for such elaborate preparations, and such an expenditure as they were at. It must, then, be something out of the house. What could it be? I thought of the assistant's fondness for photography, and his trick of vanishing into the cellar. The cellar! There was the end of this tangled clue. Then I made inquiries as to this mysterious assistant and found that I had to deal with one of the coolest and most daring criminals in London. He was doing something in the cellar--something which took many hours a day for months on end. What could it be, once more? I could think of nothing save that he was running a tunnel to some other building.
+
+"So far I had got when we went to visit the scene of action. I surprised you by beating upon the pavement with my stick. I was ascertaining whether the cellar stretched out in front or behind. It was not in front. Then I rang the bell, and, as I hoped, the assistant answered it. We have had some skirmishes, but we had never set eyes upon each other before. I hardly looked at his face. His knees were what I wished to see. You must yourself have remarked how worn, wrinkled, and stained they were. They spoke of those hours of burrowing. The only remaining point was what they were burrowing for. I walked round the corner, saw the City and Suburban Bank abutted on our friend's premises, and felt that I had solved my problem. When you drove home after the concert I called upon Scotland Yard and upon the chairman of the bank directors, with the result that you have seen."`;
